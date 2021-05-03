227b5c51c16723efdfc57922ae68e7841ef15039
randriii <noyohe6275@sumwan.com>
2021-05-04 02:09:26 +0300

    fix cross origin

--- src/main/java/com/springboot/backend/app/controller/VoitureController.java
+++ src/main/java/com/springboot/backend/app/controller/VoitureController.java
@@ -3,6 +3,7 @@ package com.springboot.backend.app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class VoitureController {
 	@Autowired
 	private VoitureRepository voitureRepository;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/voitures")
 	public List<Voiture> getAllVoitures(){
 		return voitureRepository.findAll();
